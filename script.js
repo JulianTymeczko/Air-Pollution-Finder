182398c532fb6241598f295e4b64687ec57cce12
Sam Teel <sjkteel@hotmail.com>
2023-06-12 19:50:18 -0400

    checkout out julians

--- script.js
+++ script.js
@@ -14,18 +14,22 @@
 
 
 
-
+var fetchBtn = $('#fetcher');
 // Here Sam is working out his fetch
 //airbb has alot of query items we could possibly use.
 var airBBLocal = 'paris';
-var airBBCheckIn;
-var airBBCheckOut;
+var airBBCheckIn = 2023 - 0o6 - 16;
+var airBBCheckOut = 2023 - 0o6 - 17;
 var airBBCurrency;
-var airBBAdults;
-var airBBURL = 'https://airbnb13.p.rapidapi.com/search-location?location=' + airBBLocal + '&checkin=' + airBBCheckIn + '&checkout=' + airBBCheckOut + '&adults=' + airBBAdults + '&page=1&currency=' + airBBCurrency;
-fetch(airBBURL)
-    .then(function (response) { return response.json(); })
-    .then(function (airBBData) {
-        console.log('airBB data below: ');
-        console.log(airBBData);
-    });
+var airBBAdults = 2;
+
+fetchBtn.on('click', function () {
+    var airBBURL = 'https://airbnb13.p.rapidapi.com/search-location?location=' + airBBLocal + '&checkin=' + airBBCheckIn + '&checkout=' + airBBCheckOut + '&adults=' + airBBAdults + '&page=1';
+    fetch(airBBURL)
+        .then(function (response) { return response.json(); })
+        .then(function (airBBData) {
+            console.log('airBB data below: ');
+            console.log(airBBData);
+        });
+
+})
