938e9440e470774566864ca7c03afa9ea0dd6706
Sam Teel <sjkteel@hotmail.com>
2023-06-14 20:23:54 -0400

    WIP merging

---
+++ README.md
@@ -0,0 +1,39 @@
+# Project-1
+
+## Project Name
+
+Air-/Air B&B
+
+## Project Description
+
+In a world where climate change is impacting our day to day lives in an increasing amount, soon an added step to your travels will not only be to check the relative safety of a location but also the environmental safety as air qualities change alongside our planets health.
+
+API 1 takes a user input for a city/location they want to Air B&B, which then interacts with API2 to get air quality in that city/location.
+
+Target Audience:
+Travel + Health and wellness
+
+## User Story
+
+AS a USER I want to be able to search for an air B&B city, and check its air quality.
+AS a USER I want to be able to interact with the application,
+AS a USER I want to see a polished UI, which reacts dynamically to my input.
+
+## BREAK DOWN
+
+UI design - Julian
+Air B&B - Sam
+Air Quality-Mukhtaar
+
+## TODO
+
+possible2
+[air polution](https://rapidapi.com/apidojo/api/airvisual1/) PERSON1
+possible1
+[air B&B](https://rapidapi.com/3b-data-3b-data-default/api/airbnb13/)
+Site Design PERSON3(?)
+
+API TO USE?
+
+[rapidAPI](https://rapidapi.com/hub/)
+[githubAPI](https://coding-boot-camp.github.io/full-stack/apis/api-resources)
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 
+
 // HEY SO tara and i spoke and we should create branches off of the develop branch, not main. Imagine main as an active site 
 //and we should only push when everything works in develop branch. 
 
@@ -16,15 +17,15 @@
 
 // Here Sam is working out his fetch
 //airbb has alot of query items we could possibly use.
-// var airBBLocal = 'paris';
-// var airBBCheckIn;
-// var airBBCheckOut;
-// var airBBCurrency;
-// var airBBAdults;
-// var airBBURL = 'https://airbnb13.p.rapidapi.com/search-location?location=' + airBBLocal + '&checkin=' + airBBCheckIn + '&checkout=' + airBBCheckOut + '&adults=' + airBBAdults + '&page=1&currency=' + airBBCurrency;
-// fetch(airBBURL)
-//     .then(function (response) { return response.json(); })
-//     .then(function (airBBData) {
-//         console.log('airBB data below: ');
-//         console.log(airBBData);
-//     });
+var airBBLocal = 'paris';
+var airBBCheckIn;
+var airBBCheckOut;
+var airBBCurrency;
+var airBBAdults;
+var airBBURL = 'https://airbnb13.p.rapidapi.com/search-location?location=' + airBBLocal + '&checkin=' + airBBCheckIn + '&checkout=' + airBBCheckOut + '&adults=' + airBBAdults + '&page=1&currency=' + airBBCurrency;
+fetch(airBBURL)
+    .then(function (response) { return response.json(); })
+    .then(function (airBBData) {
+        console.log('airBB data below: ');
+        console.log(airBBData);
+    });
